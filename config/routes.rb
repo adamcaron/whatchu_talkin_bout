f81d25369b6cdbbc32f8bd0451dacaa4ec584627
Adam Caron <adamwcaron@gmail.com>
2015-09-22 21:24:53 -0600

    Move old css away from current css

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
-  root 'landing#show'
+  root                               'landing#show'
+  get  '/auth/twitter/callback', to: 'sessions#create'
+  get  '/feed',                  to: 'feed#show'
+  get  '/logout',                to: 'sessions#destroy'
 
-  get '/auth/twitter/callback', to: 'sessions#create'
-  get '/logout', to: 'sessions#destroy'
-
-  get '/feed', to: 'feed#show'
 
   # get '/welcome', to: 'home#welcome'
   # get '/search_results', to: 'home#search_results'
