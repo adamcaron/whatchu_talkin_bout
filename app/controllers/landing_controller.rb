34e6892e1c596954ad086104e6edd26000e77e0e
Adam Caron <adamwcaron@gmail.com>
2015-09-22 21:32:10 -0600

    Remove old files and comments

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -2,19 +2,4 @@ class LandingController < ApplicationController
   def show
     redirect_to feed_path if current_user
   end
-
-  # def landing
-  # end
-
-  # def welcome
-  #   render :landing unless current_user
-  # end
-
-  # def search_results
-
-  # end
-
-  # def discussion
-
-  # end
 end
