9818f206d7e79b6f00554d9853731a255a615109
Adam Caron <adamwcaron@gmail.com>
2015-09-28 13:14:08 -0600

    Remove template comments

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -32,7 +32,6 @@ RSpec.describe User, type: :model do
       it "responds to 'find_or_create_by'" do
         expect(User).to respond_to(:find_or_create_by)
       end
-      # it { expect(factory_instance).to respond_to(:public_method_name) }
     end
 
     context "executes methods correctly" do
@@ -44,44 +43,8 @@ RSpec.describe User, type: :model do
           expect(user.provider).to eq("twitter")
           expect(user.nickname).to eq("adamcaron_")
         end
-        # it "does what it's supposed to..." do
-        #   expect(factory_instance.method_to_test).to eq(value_you_expect)
-        # end
       end
     end
   end
-
-  # describe "public instance methods" do
-  #   context "responds to its methods" do
-  #     it { expect(factory_instance).to respond_to(:public_method_name) }
-  #     it { expect(factory_instance).to respond_to(:public_method_name) }
-  #       t.string   "chamber"
-  #       t.string   "title"
-  #       t.string   "first_name"
-  #       t.string   "last_name"
-  #       t.string   "party"
-  #       t.string   "state"
-  #       t.string   "statename"
-  #       t.string   "twitter_id"
-  #       t.string   "website"
-  #       t.string   "district"
-  #       t.datetime "created_at", null: false
-  #       t.datetime "updated_at", null: false
-  #   end
-
-  #   context "executes methods correctly" do
-  #     context "#method name" do
-  #       it "does what it's supposed to..."
-  #         expect(factory_instance.method_to_test).to eq(value_you_expect)
-  #       end
-
-  #       it "does what it's supposed to..."
-  #         expect(factory_instance.method_to_test).to eq(value_you_expect)
-  #       end
-  #     end
-  #   end
-  # end
-
-
 end
 
