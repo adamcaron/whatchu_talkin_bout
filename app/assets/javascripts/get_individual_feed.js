cb54343520d13ca194c08df3bc4bb92d767d96f4
Adam Caron <adamwcaron@gmail.com>
2015-09-30 13:17:16 -0600

    Setup js to return an individual feed

--- app/assets/javascripts/get_individual_feed.js
+++ app/assets/javascripts/get_individual_feed.js
@@ -1,13 +1,25 @@
 $(document).ready(function(){
-  $("#legislators").delegate(".legislator > a", "click", function(){
-      console.log("monkeys");
-      var handle = $(this).data("twitter-handle").val()
-      // getIndividualFeed()
+  $("#legislators").delegate(".legislator a", "click", function(){
+      var legislatorName = $(this).data("legislator-name")
+      var handle         = $(".legislator").data("twitter-handle")
+      getIndividualFeed(legislatorName, handle)
   })
 })
 
-// function getIndividualFeed() {
-//   $.ajax({
+function getIndividualFeed(legislatorName, handle) {
+      // console.log("monkeys");
+  $.ajax({
+    type: "GET",
+    url:  "/individual_feed",
+    data: { handle: handle },
+    success: function(tweets) {
+      // Set the page title
+      $("#feed-title").text(legislatorName + "'s' recent Tweets:")
 
-//   })
-// }
+      // Render the Tweets
+      $.each(tweets, function(index, tweet){
+        renderTweet(tweet)
+      })
+    },
+  })
+}
