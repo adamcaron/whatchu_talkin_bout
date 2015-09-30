8e7664490cdb559d44d52877cdc69ade2b6eaea3
Adam Caron <adamwcaron@gmail.com>
2015-09-30 12:12:02 -0600

    Wait for the elements to load before selecting them

--- app/assets/javascripts/get_individual_feed.js
+++ app/assets/javascripts/get_individual_feed.js
@@ -1,12 +1,10 @@
-// $( ".legislator" ).ready(function(){
-
-  $( "#legislators").on("click", "a", function() {
-    console.log("monkeys");
-    debugger;
-    // var handle = $(this).data("twitter-handle").val()
-    // getIndividualFeed()
+$(document).ready(function(){
+  $("#legislators").delegate(".legislator > a", "click", function(){
+      console.log("monkeys");
+      var handle = $(this).data("twitter-handle").val()
+      // getIndividualFeed()
   })
-// })
+})
 
 // function getIndividualFeed() {
 //   $.ajax({
