c53d83dbdb39bd4c46eedc9b7f1e81206086b763
Adam Caron <adamwcaron@gmail.com>
2015-09-29 19:12:22 -0600

    Setup individual feed call

---
+++ app/assets/javascripts/get_individual_feed.js
@@ -0,0 +1,15 @@
+// $( ".legislator" ).ready(function(){
+
+  $( "#legislators").on("click", "a", function() {
+    console.log("monkeys");
+    debugger;
+    // var handle = $(this).data("twitter-handle").val()
+    // getIndividualFeed()
+  })
+// })
+
+// function getIndividualFeed() {
+//   $.ajax({
+
+//   })
+// }
